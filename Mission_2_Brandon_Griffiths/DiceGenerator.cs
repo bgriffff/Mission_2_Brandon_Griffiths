27ebbced0122a8ee83fbfb00eb8715df7c2ac72a
bgriffff <brandonjosephgriffiths@gmail.com>
2023-01-17 20:40:01 -0700

    first commit

---
+++ Mission_2_Brandon_Griffiths/DiceGenerator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mission_2_Brandon_Griffiths
+{
+    class DiceGenerator
+    {
+        public void DiceCount (int diceRolls)
+        {
+            Random rnd = new Random();   
+            
+            int[] rolls;
+
+            rolls = new int[11];
+
+            // gets the random numbers
+            for (int j = 0; j < diceRolls; j++)
+            {
+                int ranNum = 0;
+                int ranNum2 = 0;
+                int total = 0;
+
+                ranNum = rnd.Next(6) + 1;
+                ranNum2 = rnd.Next(6) + 1;
+                total = ranNum + ranNum2;
+
+
+                // adding 1 to elements of the array
+                if (rolls[total - 2] >= 1)
+                {
+                    rolls[total - 2]++;
+                }
+                else
+                {
+                    rolls[total - 2] = 1;
+                }
+
+            }
+
+            string[] stars = new string[11];
+
+            // creating the *'s
+            for (int i = 0; i < rolls.Length; i++)
+            {
+                int number = 0;
+
+                int num = 0;
+
+                number = (i + 2);
+
+                num = rolls[i];
+
+                rolls[i] = (int)Math.Round(((double)num / diceRolls) * 100);
+
+                // adding the number of stars to the string
+                for (int n = 0; n < rolls[i]; n++)
+                {
+                    stars[i] += '*';
+                }
+
+
+                Console.WriteLine(number + ": " + stars[i]);
+            }
+
+        }
+    }
+}
---
+++ Mission_2_Brandon_Griffiths/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Mission_2_Brandon_Griffiths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DiceGenerator dg = new DiceGenerator();
+
+            Console.WriteLine("Welcome to the dice throwing simulator!\n");
+
+            Console.WriteLine("How many dice rolls would you like to simulate?\n");
+
+            int diceRolls = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("DICE ROLLING SIMULATION RESULTS:");
+
+            dg.DiceCount(diceRolls);
+
+
+            Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+
+
+
+        }
+    }
+}
